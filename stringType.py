cc202b65d971616d2db871d149712ff995f06ce9
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-04-21 15:19:19 +0300

    new version

---
+++ stringType.py
@@ -0,0 +1,18 @@
+# Declare a immutable string object
+name = 'anyText'
+
+# Declare a ney string object with the same name and assign a new value
+name = 'new' + name[3:]
+
+# Check string object length
+len(name)
+
+# Take the first char from string object
+firstLetter = name[0]
+
+# Take the last char from string object
+lastLetter = name[-1]
+
+# Slice a range of chars from string object (last char not included)
+sliceChars = name[0:3]
+
