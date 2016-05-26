f6894122a59d73398f94a42ea0e4365f719de64c
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-05-26 19:40:20 +0400

    Update dictType.py

--- dictType.py
+++ dictType.py
@@ -14,6 +14,9 @@ else:
 # Check dict if key is present else assign a new value
 value = dict1['lastName'] if 'lastName' in dict1 else 'none'
 
+# Zipping two lists in one dictionary
+zipped = dict(zip(['name', 'job', 'age'], ['Sergey', 'IT', '34']))
+
 # Sort dict by key value
 for key in sorted(dict3):
     print(key, '=', dict3[key])
