134e74e0a00e03944377aa43acc681bcecbadd16
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-05-31 12:17:46 +0400

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,8 +1,11 @@
-A = Y if X else Z                         # ternary expression
-A = [Y, Z][bool(X)]                       # bool function with offset
-A = X or Y or None                        # assings first nonempty object or None
+A = Y if X else Z                             # ternary expression
+A = [Y, Z][bool(X)]                           # bool function with offset
+A = X or Y or None                            # assings first nonempty object or None
 
 A = [(1, 2), (3, 4), (5, 6)]
 for (a, b) in A:
   print(a, b)
+
+for (a, b, c) in [(1, 2, 3), (4, 5, 6)]:      # tuple in the loop
+  print(a, b, c)
 
