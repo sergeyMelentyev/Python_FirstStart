40101cf3c90f9a7f313bb520e48095ec4b890ec3
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-06-03 15:11:33 +0400

    Update def.py

--- def.py
+++ def.py
@@ -1,12 +1,21 @@
-def makerNested(N):
-  def action(X):
-    return X ** N
+def makerNested(n):
+  def action(x):
+    return x ** n
   return action
 
-def makerLambda(N):
-  return lambda X : X ** N
+def makerLambda(n):
+  return lambda x : x ** n
 
 f = makerNested(2)
 h = makerLambda(2)
 f(3)                                              # 9
 h(3)                                              # 9
+
+def func():
+  x = 4
+  action = (lambda n: x ** n)
+  return action
+
+x = func()
+x(2)                                              # 16
+
