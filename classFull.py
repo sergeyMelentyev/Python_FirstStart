6131b209711b4cc0cfae38663c0e898590f79831
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-06-09 17:06:09 +0400

    Update classFull.py

--- classFull.py
+++ classFull.py
@@ -1,19 +1,19 @@
 # PIZZA SHOP INTERFACE
-class Employee:																				# declare a super class
+class Employee:										# declare a super class
 	"""docstring for Employee"""
-	def __init__(self, name, salary=0):														# custom init overloader
-		self.name = name																	# class attribute
+	def __init__(self, name, salary=0):						# custom init overloader
+		self.name = name							# class attribute
 		self.salary = salary
-	def giveRise(self, percent):															# class method
+	def giveRise(self, percent):							# class method
 		self.salary = self.salary + (self.salary * percent)
 	def work(self):
 		print(self.name, 'does stuff')
 
-class Chef(Employee):																		# sub class
+class Chef(Employee):									# sub class
 	"""docstring for Chef"""
-	def __init__(self, name):																# custom init overloader
+	def __init__(self, name):							# custom init overloader
 		Employee.__init__(self, name, 50000)
-	def work(self):																			# custom method overloader
+	def work(self):									# custom method overloader
 		print(self.name, 'makes food')
 
 class Server(Employee):
@@ -31,18 +31,18 @@ class PizzaRobot(Chef):
 		print(self.name, 'makes pizza')
 
 # CUSTOMER INTERFACE
-class Customer:																				# declare a new super class
+class Customer:										# declare a new super class
 	"""docstring for Customer"""
-	def __init__(self, name):																# custom init overloader
+	def __init__(self, name):							# custom init overloader
 		self.name = name
-	def order(self, server):																# class method
+	def order(self, server):							# class method
 		print(self.name, 'order from', server)
 	def pay(self, server):
 		print(self.name, 'pays for item to', server)
 
-class Oven:																					# declare a new super class
+class Oven:										# declare a new super class
 	"""docstring for Oven"""
-	def bake(self):																			# custom init overloader
+	def bake(self):									# custom init overloader
 		print('oven bakes')
 
 class PizzaShop:
